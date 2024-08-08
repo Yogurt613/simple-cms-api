88f44340fd588087d0cb9edc8bac5af10fcf1140
Stella <hanhanyang399@gmail.com>
2024-08-08 21:10:07 +0800

    Add todo api endpoints

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express');    
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// 測試端點
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello, World!' });
 });
 
+// 取得所有客戶
+app.get('/api/customers', (req, res) => {
+  // TODO
+});
+
+// 取得單個客戶
+app.get('/api/customers/:id', (req, res) => {
+  // TODO
+});
+
+// 建立客戶
+app.post('/api/customers', (req, res) => {
+  // TODO
+});
+
+// 更新客戶
+app.put('/api/customers/:id', (req, res) => {
+  // TODO
+});
+
+// 刪除客戶
+app.delete('/api/customers/:id', (req, res) => {
+  // TODO
+});
+
+// 啟動伺服器
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
